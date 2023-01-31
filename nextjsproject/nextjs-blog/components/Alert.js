ece941e43ac339d601af69ab65c370a9002368cb
donetian-petkov <73138116+donetian-petkov@users.noreply.github.com>
2023-01-31 09:02:53 +0200

    Global CSS Next.js

---
+++ nextjsproject/nextjs-blog/components/Alert.js
@@ -0,0 +1,18 @@
+import styles from './Alert.module.css';
+import { clsx } from 'clsx';
+
+export default function Alert({
+                                  children,
+                                  type
+}) {
+    return (
+        <div
+            className={clsx({
+                [styles.success]: type === 'success',
+                [styles.error]: type === 'error',
+            })}
+        >
+            {children}
+        </div>
+    );
+}
