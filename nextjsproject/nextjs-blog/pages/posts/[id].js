23096fe83967cbd807fedfdce513f1e2f016b4f0
donetian-petkov <73138116+donetian-petkov@users.noreply.github.com>
2023-02-05 23:17:31 +0200

    Dynamic Routes Next.js

--- nextjsproject/nextjs-blog/pages/posts/[id].js
+++ nextjsproject/nextjs-blog/pages/posts/[id].js
@@ -1,9 +1,24 @@
 import Layout from '../../components/layout';
-import { getAllPostIds } from '../../lib/posts';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 
 
-export default function Post() {
-    return <Layout>...</Layout>;
+
+export default function Post({postData}) {
+    return  <Layout>
+        <Head>
+        {postData.title}
+        </Head>
+        <article>
+            <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+            <div className={utilStyles.lightText}>
+                <Date dateString={postData.date} />
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
+    </Layout>;
 }
 
 export async function getStaticPaths() {
@@ -11,5 +26,14 @@ export async function getStaticPaths() {
     return {
         paths,
         fallback: false,
+    };
+}
+
+export async function getStaticProps({ params }) {
+    const postData = await getPostData(params.id);
+    return {
+        props: {
+            postData,
+        },
     };
 }
